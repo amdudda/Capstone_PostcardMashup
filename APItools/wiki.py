463d66a42429b66e1744abcbc28dda03ef15dcbf
marian <marian@email.com>
2016-11-17 10:12:16 -0600

    handling exception and igrnone the warm

--- APItools/wiki.py
+++ APItools/wiki.py
@@ -1,6 +1,7 @@
 from wikipedia import wikipedia
 from contextlib import suppress
 import warnings
+import random
 import requests
 
 class wikipedia_API:
@@ -10,25 +11,36 @@ class wikipedia_API:
 #todo you need to figure out how to deal with less specific search term
     #web scrapes wikidedia page for search term then get one sentence of summary
     def get_wiki_snippet(self, search_term):
-        result = wikipedia.summary(search_term, sentences=self.sentence)
+        #https://docs.python.org/3/library/warnings.html#temporarily-suppressing-warnings the link to chatch warning
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            try:
+                result = wikipedia.summary(search_term, sentences=self.sentence)
+            except wikipedia.DisambiguationError as e:
+
+                the = random.choice(e.options)
+                result = wikipedia.summary(the, sentences=self.sentence)
+
+
         return result
 
-# debugging
-if __name__ == '__main__':
-    # re = wikipedia.page("Thanksgiving")
-    # rs = wikipedia.search("thanksgiving")
-    th = None
 
-    # this isgnores all warnings raised - this is not without risks - see https://docs.python.org/2/library/warnings.html
-    warnings.filterwarnings('ignore')
-    try:
-        # with suppress(UserWarning):
-        th = wikipedia.summary('thing', sentences=3)
-        # print(th)
-    except wikipedia.DisambiguationError as de:
-        th = ("A disambiguation page was reached, with the following choices:\n" + str(de.options))
-    finally:
-        print(th)
+# debugging
+# if __name__ == '__main__':
+#     # re = wikipedia.page("Thanksgiving")
+#     # rs = wikipedia.search("thanksgiving")
+#     th = None
+#
+#     # this isgnores all warnings raised - this is not without risks - see https://docs.python.org/2/library/warnings.html
+#     warnings.filterwarnings('ignore')
+#     try:
+#         # with suppress(UserWarning):
+#         th = wikipedia.summary('thing', sentences=3)
+#         # print(th)
+#     except wikipedia.DisambiguationError as de:
+#         th = ("A disambiguation page was reached, with the following choices:\n" + str(de.options))
+#     finally:
+#         print(th)
 
 
 
