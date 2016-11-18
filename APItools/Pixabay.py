7cf557fa486857f3bac5704af5f00c1b5f1d1e9e
amdudda <tk0654wm@minneapolis.edu>
2016-11-17 19:15:30 -0600

    Recycle code from another project to download and save images.

--- APItools/Pixabay.py
+++ APItools/Pixabay.py
@@ -4,6 +4,7 @@ This will eventually hold code that returns a url to an image on Pixabay.
 import requests # http://docs.python-requests.org/en/master/
 import json
 import random
+import urllib.request, datetime
 
 
 def get_image_url(search_for="Thanksgiving", pixabaykey=None):
@@ -34,7 +35,32 @@ def get_image_url(search_for="Thanksgiving", pixabaykey=None):
         # if no results found, return None so code can inspect for that.
         return None
 
+def save_image(imageurl):
+    ''' takes a url as an argument and tries to save the image to a directory'''
+
+    def timestamp():
+        stamp = datetime.datetime.now()
+        ts = str(stamp.date()) + "-" + str(stamp.hour) + "-" + str(stamp.minute)
+        return ts
+
+    startfrom = imageurl.rfind("/") + 1
+
+    ts = timestamp()
+    filename = ts + "_" + imageurl[startfrom:]
+    print(filename)
+
+    filepath = "images/" + filename
+
+    try:
+        urllib.request.urlretrieve(imageurl, filepath)
+        return True
+    except (urllib.error.HTTPError) as exc:
+        print("An error occurred: " + str(exc))
+        return False
+
+
 # debugging
 if __name__ == "__main__":
     img_url = get_image_url()
     print(img_url)
+    save_image(img_url)
