758743b007b348fb41e6e884a62b5ae9450402e5
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-11-23 15:53:00 -0500

    #views coded

--- PostcardMashup/PostcardApp/views.py
+++ PostcardMashup/PostcardApp/views.py
@@ -15,30 +15,17 @@ def index(request):
         tweet_txt = Postcard_data.append(get_tweet(search))
         Postcard_items = mod.objects.create(image=Pixabay_Image, wiki_sentence=Wiki_snip, tweet_text=tweet_txt )
         Postcard = mod.objects.all()
-        return render(request, 'PostcardApp/index.html')
+        return render(request, 'PostcardApp/index.html', {'Postcard': Postcard})
     elif request.method == 'GET':
-        search_keyword = request.GET('search')
+        search_keyword = request.GET.get('search')
         Postcard_data = []
         Pixabay_Image = Postcard_data.append(get_image(search_keyword))
         Wiki_snip = Postcard_data.append(get_wiki_content(search_keyword))
         tweet_txt = Postcard_data.append(get_tweet(search))
         Postcard_items = mod.objects.create(image=Pixabay_Image, wiki_sentence=Wiki_snip, tweet_text=tweet_txt)
         Postcard = mod.objects.all()
-        return render(request, 'PostcardApp/index.html')
+        return render(request, 'PostcardApp/index.html', {'Postcard': Postcard})
 
 
-# def result(request):
-#     Postcard_data = []
-#     search_keyword=''
-#     if request.method == 'POST':
-#         search_keyword = request.POST.get('search')
-#     elif request.method == "GET":
-#         search_keyword = request.GET('search')
-#     pixabay_image = get_image(search_keyword)
-#     Wiki_snip = (get_wiki_content(search_keyword))
-#     tweet_txt = (get_tweet(search))
-#     Postcard_data.append(pixabay_image)
-#     Postcard_data.append(Wiki_snip)
-#     Postcard_data.append(tweet_txt)
-#     return Postcard_data
+
 
