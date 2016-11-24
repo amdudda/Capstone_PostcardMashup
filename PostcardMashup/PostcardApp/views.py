d3ce7bde3fd3eecfd11595cf1055b0e78d683e03
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-11-23 20:24:49 -0500

    fixed none object error for submit button

--- PostcardMashup/PostcardApp/views.py
+++ PostcardMashup/PostcardApp/views.py
@@ -13,14 +13,21 @@ def index(request):
     # ORM queries the database for all of the to-do entries.
     if request.method == 'GET':
         return render(request, 'PostcardApp/index.html')
+    elif request.method == 'POST':
+            search_keyword = request.POST.get('search')
+            Postcard_items = mod.objects.create(image=result(search_keyword)[0],
+                                                wiki_sentence=result(search_keyword)[1],
+                                                tweet_text=result(search_keyword)[2])
+            Postcard = mod.objects.all()
+            return render(request, 'PostcardApp/index.html', {'Postcard': Postcard})
 
 
-def result_page(request):
-    if request.method == 'POST':
-        search_keyword = request.POST.get('search')
-        Postcard_items = mod.objects.create(image=result(search_keyword)[0], wiki_sentence=result(search_keyword)[1], tweet_text=result(search_keyword)[2])
-        Postcard = mod.objects.all()
-        return render(request, 'PostcardApp/index.html', {'Postcard': Postcard})
+# def result_page(request):
+#     if request.method == 'POST':
+#         search_keyword = request.POST.get('search')
+#         Postcard_items = mod.objects.create(image=result(search_keyword)[0], wiki_sentence=result(search_keyword)[1], tweet_text=result(search_keyword)[2])
+#         Postcard = mod.objects.all()
+#         return render(request, 'PostcardApp/index.html', {'Postcard': Postcard})
 
 
 def result(search_keyword):
