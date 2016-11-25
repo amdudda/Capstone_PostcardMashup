2dcd937b747ed80d940ec8eecc7900de00a4caf2
amdudda <tk0654wm@minneapolis.edu>
2016-11-25 08:13:13 -0600

    Enable utf-8 encoding of wiki snippets and misc cleanup.

--- PostcardMashup/PostcardApp/views.py
+++ PostcardMashup/PostcardApp/views.py
@@ -10,38 +10,42 @@ def index(request):
         return render(request, 'PostcardApp/index.html')
     elif request.method == 'POST':
             search_keyword = request.POST.get('search')
-            # p_card_data = result(search_keyword)
+            # use threading to speed up searching
             p_card_data = threaded_search(search_keyword)
+            # if pixabay didn't return anything, substitute our default image instead
+            # TODO create a default image
+            # encoding our data as utf-8 lets us cope with non-ascii text...
+            if not p_card_data['image']: p_card_data['image'] = "/images/no_results.jpg"
             Postcard_items = mod.objects.create(image=p_card_data['image'],
-                                                wiki_sentence=p_card_data['wiki'],
-                                                tweet_text=p_card_data['tweet'].encode('utf-8') # encoding lets us cope with non-ascii text...
+                                                wiki_sentence=p_card_data['wiki'].encode('utf-8'),
+                                                tweet_text=p_card_data['tweet'].encode('utf-8')
                                                 )
-            Postcard = mod.objects.all()
-            # TODO for some reason this is sending ALL postcards instead of just the one we're creating...
+            # Postcard = mod.objects.all()
+            # return the newly-created postcard
             return render(request, 'PostcardApp/index.html', {'Postcard': Postcard_items})
 
-
-def result(search_keyword):
-    # TODO take advantage of threading in API_Manager via threaded_search
-    Postcard_data = []
-    Pixabay_Image = get_image(search_keyword)
-    Wiki_snip = get_wiki_content(search_keyword)
-    tweet_txt = (get_tweet(search_keyword))
-    if Pixabay_Image:
-        Postcard_data.append(Pixabay_Image)
-    else:
-        Postcard_data.append("No image found")
-
-    if Wiki_snip:
-        Postcard_data.append(Wiki_snip)
-    else:
-        Postcard_data.append("No wiki snippet found")
-    if tweet_txt:
-        Postcard_data.append(tweet_txt) # get a tweet
-    else:
-        Postcard_data.append("No tweet found")
-
-    return Postcard_data
+# AMD: I think my changes make this obsolete!  Now with threading!  :-D
+# def result(search_keyword):
+#     # DONE: take advantage of threading in API_Manager via threaded_search
+#     Postcard_data = []
+#     Pixabay_Image = get_image(search_keyword)
+#     Wiki_snip = get_wiki_content(search_keyword)
+#     tweet_txt = (get_tweet(search_keyword))
+#     if Pixabay_Image:
+#         Postcard_data.append(Pixabay_Image)
+#     else:
+#         Postcard_data.append("No image found")
+#
+#     if Wiki_snip:
+#         Postcard_data.append(Wiki_snip)
+#     else:
+#         Postcard_data.append("No wiki snippet found")
+#     if tweet_txt:
+#         Postcard_data.append(tweet_txt) # get a tweet
+#     else:
+#         Postcard_data.append("No tweet found")
+#
+#     return Postcard_data
 
 
 
